62029bd44f1c058e3691d2dd9cf290b03860484a
Anil kumar <anilk3538@gmail.com>
2021-04-11 15:23:54 +0530

    init

---
+++ main.js
@@ -0,0 +1,35 @@
+const navslider = () =>{
+    const icon = document.querySelector('.menuicon');
+    const navmenu = document.querySelector('.navlist');
+
+    icon.addEventListener('click', ()=>{
+        
+        navmenu.classList.toggle('nav-active');
+        navmenu.style.transition = "transform 0.4s ease-in";
+        
+    });
+
+    document.addEventListener('click', (e)=>{      
+                    //removes the navigation container and the icon animation on clicking anywhere    //here if the condition is true two events will occur which are passed in a () separated by , 
+    !icon.contains(e.target)? (navmenu.classList.remove('nav-active') , icon.classList.remove('opened')) : '' ; //ternary operator
+
+})
+
+}
+navslider();
+
+const sendMail = () =>{
+    const mydata = {
+        name: document.querySelector('#empname').value,
+        mail: document.querySelector('#empmail').value,
+        message: document.querySelector('#empmessage').value,
+    };
+
+    emailjs.send('gmail','template_9duzy5n',mydata).then(res => console.log('submitted' , res.status));
+}
+
+document.querySelector('#submitbtn').addEventListener('click', () =>{
+    sendMail();
+    alert('your Message has been Successfully Sent');
+})
+
